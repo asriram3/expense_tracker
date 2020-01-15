77988b63650a55288230284a90e8adb958c68b8d
Adi <asriram3@illinois.edu>
2020-01-16 02:19:04 +0800

    styled transaction lit items

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -27,8 +28,8 @@ class MyHomePage extends StatelessWidget {
     ),
     Transaction(
       id: 't2',
-      title: 'Electric Guitar',
-      amount: 165.8,
+      title: 'Guitar Stand',
+      amount: 15.8,
       date: DateTime.now(),
     ),
   ];
@@ -43,13 +44,66 @@ class MyHomePage extends StatelessWidget {
         title: Text('Expense Tracker'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Card(
-            child: Text('CHART'),
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text(
+                'CHART',
+                textAlign: TextAlign.center,
+              ),
+            ),
           ),
-          Card(
-            child: Text('LIST OF TX'),
-          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 20,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.orange, width: 2),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$${tx.amount}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.orange,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Container(
+                          child: Text(
+                            DateFormat.yMMMd().format(tx.date),
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          )
         ],
       ),
     );
